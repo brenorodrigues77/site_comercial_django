5acdb0df5c6183bceef966bf70d5ef76a51c4f5a
brenorodrigues77 <breno7rodrigues@gmail.com>
2025-07-30 14:49:36 -0300

    formatação do front e ajustes no painel admin
    Nesse commit foi feito a padronização de texto do footer no front-end e ajustes de imagem no painel de administração, no back-end

--- home/admin.py
+++ home/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from home.models import Home, Job
 
 
 class HomeAdmin(admin.ModelAdmin):
-    list_display = ("name", "description", "image")
+
+    def image_tag(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" style="max-width: 100px; max-height: 100px;" />'.format(obj.image.url))
+        return "-"
+
+    image_tag.short_description = "Imagem"
+
+    list_display = ("name", "description", "image_tag", )
     search_fields = ("name",)
 
 
