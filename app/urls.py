41e338e19b154470f5dcc8c0debe9f273023ba5e
brenorodrigues77 <breno7rodrigues@gmail.com>
2025-08-06 23:37:46 -0300

    estilização e ajustes
    nesse Commit foi feito ajustes de estilização e adicionado um carrosel para amostragem de imagens interativas.

--- app/urls.py
+++ app/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.home, name="home"),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
